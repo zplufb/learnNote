67cdc53cde7a55659b465a297174ae3fae54af60
lufb <zplufb@163.com>
2019-08-13 16:54:20 +0800

    add controller and server

---
+++ go/scrapy/gocollyDemo/tvmao/controller/searchController.go
@@ -0,0 +1,44 @@
+package searchController
+
+import (
+		"fmt"
+	"net/http"
+	"Test001/golang/scrapy/gocollyDemo/tvmao/server"
+	"Test001/golang/scrapy/gocollyDemo/utils"
+)
+
+func Search(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	// 允许访问所有域
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") // header的类型
+	w.Header().Set("content-type", "application/json")             // 返回数据格式是JSON
+
+	switch r.Method {
+	case "GET":
+		{
+			key := r.FormValue("key")
+
+			if key == ""{
+				resp_json := CollyUtils.GetRespJson(400, "参数有误！", nil)
+				fmt.Fprintln(w, string(resp_json))
+				return
+			}
+
+			ok := searchServer.StartSearch(key)
+			if ok {
+				resp_json := CollyUtils.GetRespJson(200, "下载成功！", nil)
+				fmt.Fprintln(w, string(resp_json))
+				return
+			}
+			resp_json := CollyUtils.GetRespJson(500, "服务器错误！", nil)
+			fmt.Fprintln(w, string(resp_json))
+			return
+		}
+	default:
+		resp_json := CollyUtils.GetRespJson(300, "错误的请求方式！", nil)
+		fmt.Fprintln(w, string(resp_json))
+		return
+	}
+}
--- go/scrapy/gocollyDemo/tvmao/main.go
+++ go/scrapy/gocollyDemo/tvmao/main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-			"Test001/golang/scrapy/gocollyDemo/tvmao/sample"
+	"net/http"
+	"fmt"
+	"Test001/golang/scrapy/gocollyDemo/tvmao/sample"
+	"Test001/golang/scrapy/gocollyDemo/tvmao/controller"
 )
 
+func init() {
+	http.HandleFunc("/query", searchController.Search)
+
+	h := http.FileServer(http.Dir("D:/Go-Work/src/Test001/golang/scrapy/gocollyDemo/tvmao/web/"))
+	http.Handle("/", h)
+}
+
 func main() {
-	//StartCollect()
+
+	startServer()
+	//demo()
+
+}
+
+func startServer() {
+	// 开启web服务器，并监听8092端口
+	err := http.ListenAndServe("0.0.0.0:50001", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Start Server Running On IP localhost:50001")
+}
+
+func demo() {
 	//搜索获取其相关信息
 	key := "九州缥缈录"
 	urlDrama := tvmao.Search(key)
 	savePathDir := "E:/ZTestData/TVMao/"
-	tvmao.StartCollect(urlDrama,savePathDir)
+	tvmao.StartCollect(urlDrama, savePathDir)
 }
